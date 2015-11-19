8d41af8e6b88d4ea86e648488715df40cc71ce41
tailec <pawel@tailec.com>
2015-11-19 22:59:06 +0000

    Remove ignored files

--- reactive-recipe-1/Pods/Target Support Files/Pods-reactive-recipe-1/Pods-reactive-recipe-1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reactive_recipe_1 : NSObject
-@end
-@implementation PodsDummy_Pods_reactive_recipe_1
-@end
--- reactive-recipe-1/Pods/Target Support Files/Pods-reactive-recipe-1/Pods-reactive-recipe-1-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_reactive_recipe_1VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_reactive_recipe_1VersionString[];
-
